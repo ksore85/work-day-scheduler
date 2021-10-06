eb39fe62eb38a3deb15aebddc173c7f26103e382
ksore85 <84486015+ksore85@users.noreply.github.com>
2021-10-05 22:05:15 -0500

    Save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-// Show current date
+// Show current date/time
 const currentHour = moment().format("k");
 const currentDate = moment().format("dddd, MMMM Do, YYYY");
 const currentDateElement = document.querySelector("#currentDay");
 currentDateElement.textContent = currentDate;
-
+let tasksArray = []
 
 $(document).on("click", ".task-item", function (event) { 
     event.preventDefault();
@@ -38,7 +38,7 @@ $(document).on("blur", ".edit-task-item", function () {
     $(this).replaceWith(taskTd);
 });
 
-// background color 
+// background color change by hour
 function changeColor() {
     let hrDay = $(".time-block") 
     for (let i = 1; i < 10; i++) {
@@ -51,6 +51,46 @@ function changeColor() {
             } else if (currentHrInt < hrDayIdInt) {
                 hrDay[i].setAttribute('class', 'time-block future')
             }
+    }}
+}
+
+// Save Task Items to local storage
+$(".saveBtn").click(function () {
+    $(this)
+    .find("span:first")
+    .removeClass("oi oi-lock-unlocked")
+    .addClass("oi oi-lock-locked");
+
+    let addTask = $(this).siblings(".task-item").text();
+    let taskHour = $(this).closest("tr").attr("id");
+   
+    let tasksObject = {
+        task: addTask,
+        time: taskHour
     }
+
+    if (localStorage.getItem("tasksTable")) {
+        tasksArray = JSON.parse(localStorage.getItem("tasksTable"))
+    }
+    tasksArray.push(tasksObject)
+    localStorage.setItem("tasksTable", JSON.stringify(tasksArray))
+    localStorage.setItem("task", addTask);
+    localStorage.setItem("time", taskHour);
+    tasksArray.sort();
+});
+
+function getTasks() {
+    let showTasksObject = JSON.parse(localStorage.getItem("tasksTable"))
+    for (let i = 0; i < showTasksObject.length; i++) {
+        const text = showTasksObject[i].task;
+        const hour = showTasksObject[i].time;
+        $(`#${hour}`)
+            .find("td:first")
+            .text(text)
+        }
 }
-}
+
+// Invoke Functions
+changeColor();
+
+getTasks();
